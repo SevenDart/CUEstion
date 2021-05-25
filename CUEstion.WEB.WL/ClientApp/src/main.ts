2bf960289dff2a9970e6a22ed015e90da3d1f31e
Pavel Slyaz <60967391+SevenDart@users.noreply.github.com>
2021-05-25 12:24:16 +0300

    Delete CUEstion.WEB.WL directory

--- CUEstion.WEB.WL/ClientApp/src/main.ts
+++
@@ -1,7 +0,0 @@
-﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
-
-enableProdMode();
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
