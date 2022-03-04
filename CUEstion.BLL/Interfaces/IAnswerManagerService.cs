1437111a181116c3df12240bdc2519063335b30e
Pavel Slyaz <sir.sevendart@gmail.com>
2022-03-04 08:47:31 +0300

    global BLL refactoring

---
+++ CUEstion.BLL/Interfaces/IAnswerManagerService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CUEstion.BLL.ModelsDTO;
+
+namespace CUEstion.BLL.Interfaces
+{
+    public interface IAnswerManagerService
+    {
+        public Task<IEnumerable<AnswerDTO>> GetAnswers(int questionId);
+        public Task CreateAnswer(AnswerDTO answerDto, int questionId);
+        public Task UpdateAnswer(AnswerDTO answerDto);
+        public Task DeleteAnswer(int answerId);
+        public Task MarkAnswer(int userId, int answerId, int mark);
+    }
+}
